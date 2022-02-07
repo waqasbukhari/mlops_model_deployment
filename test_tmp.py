635c5ea60485cd1f3ffbaa35679836488d9d701c
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-07 16:46:51 +0300

    some changes

--- test_tmp.py
+++ test_tmp.py
@@ -1,7 +1,14 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def test_X():
     data_path = "modified_census.csv"
     data = pd.read_csv(data_path)
-    assert len(data.shape) == 2
+    train, _ = train_test_split(data, test_size=0.20)
+    assert len(train.shape) == 2
+    # X, _, _, _ = process_data(
+    #     train, categorical_features=CAT_FEATURES, label="salary",
+    # training=True
+    # )
+    # return X

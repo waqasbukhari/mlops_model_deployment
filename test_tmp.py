b9cf59e1daab80055f15d375ff69fa311e046045
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-07 16:53:39 +0300

    ese

--- test_tmp.py
+++ test_tmp.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from data import process_data
+
+
+CAT_FEATURES = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
 
 
 def test_X():
     data_path = "modified_census.csv"
     data = pd.read_csv(data_path)
     train, _ = train_test_split(data, test_size=0.20)
-    assert len(train.shape) == 2
-    # X, _, _, _ = process_data(
-    #     train, categorical_features=CAT_FEATURES, label="salary",
-    # training=True
-    # )
-    # return X
+    X, _, _, _ = process_data(
+                              train,
+                              categorical_features=CAT_FEATURES,
+                              label="salary",
+                              training=True)
+    assert len(X.shape) == 2

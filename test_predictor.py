fbd89540515e0ec908726bc0109904d20da8587e
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-07 18:40:24 +0300

    changed tests

--- test_predictor.py
+++ test_predictor.py
@@ -19,57 +19,35 @@ CAT_FEATURES = [
 ]
 
 
-@pytest.fixture(scope="module")
-def X():
-    data_path = "modified_census.csv"
-    data = pd.read_csv(data_path)
-    train, _ = train_test_split(data, test_size=0.20)
-    X, _, _, _ = process_data(
-        train, categorical_features=CAT_FEATURES, label="salary", training=True
-    )
-    return X
+data_path = "modified_census.csv"
+data = pd.read_csv(data_path)
+train, _ = train_test_split(data, test_size=0.20)
+X, y, _, _ = process_data(
+                          train,
+                          categorical_features=CAT_FEATURES,
+                          label="salary", training=True    )
+
+model = DummyClassifier()
+model.fit(X, y)
+preds = inference(model, X)
 
 
-@pytest.fixture(scope="module")
-def y():
-    data_path = "modified_census.csv"
-    data = pd.read_csv(data_path)
-    train, _ = train_test_split(data, test_size=0.20)
-    _, y, _, _ = process_data(
-        train, categorical_features=CAT_FEATURES, label="salary", training=True
-    )
-    return y
-
-
-@pytest.fixture(scope="module")
-def model(X, y):
-    dummy = DummyClassifier()
-    dummy.fit(X, y)
-    return dummy
-
-
-@pytest.fixture(scope="module")
-def preds(model, X):
-    preds = inference(model, X)
-    return preds
-
-
-def test_compute_model_metrics_count(preds, y):
+def test_compute_model_metrics_count():
     metrics = compute_model_metrics(y, preds)
     assert len(metrics) == 3
 
 
-def test_compute_model_metrics_range(y, preds):
+def test_compute_model_metrics_range():
     metrics = compute_model_metrics(y, preds)
     result = map((lambda m: (m >= 0) & (m <= 1)), metrics)
     assert all(result)
 
 
-def test_inference_shape(model, X):
+def test_inference_shape():
     preds = inference(model, X)
     assert len(preds) == X.shape[0]
 
 
-def test_inference_values(model, X):
+def test_inference_values():
     preds = inference(model, X)
     assert np.all((preds == 0) | (preds == 1))

d39cd4e00ceb1bb4e945f822ab5bfe452721de3d
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-08 14:20:44 +0300

    secfc

--- test_tmp.py
+++ test_tmp.py
@@ -1,21 +1,4 @@
 import pandas as pd
-from sklearn.dummy import DummyClassifier
-from sklearn.model_selection import train_test_split
-
-from data import process_data
-from model import inference
-
-CAT_FEATURES = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-
 
 data_path = "modified_census.csv"
 data = pd.read_csv(data_path)
@@ -23,16 +6,3 @@ print("Salary column in modified_census.csv")
 print("salary" in data.columns)
 print(data.columns)
 print()
-train, _ = train_test_split(data, test_size=0.20)
-X, y, _, _ = process_data(
-                          train,
-                          categorical_features=CAT_FEATURES,
-                          label="salary", training=True)
-
-model = DummyClassifier()
-model.fit(X, y)
-preds = inference(model, X)
-
-
-def test_y():
-    assert data.columns[-1] == 'salary'

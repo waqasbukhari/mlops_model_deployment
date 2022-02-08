231dad974383c70dd730282c0d7ede911c89d24e
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-08 14:27:09 +0300

    sedhfuerf

--- test_tmp.py
+++ test_tmp.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 data_path = "modified_census.csv"
 data = pd.read_csv(data_path)
+print(data.head())
+print()
+print(type(data))
+print()
 print("Salary column in modified_census.csv")
 print("salary" in data.columns)
 print(data.columns)

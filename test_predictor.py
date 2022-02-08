e44bfae65236f0c8fbf501e76b3aaaad8989d5ca
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-08 12:42:59 +0300

    se

--- test_predictor.py
+++ test_predictor.py
@@ -20,6 +20,8 @@ CAT_FEATURES = [
 
 data_path = "modified_census.csv"
 data = pd.read_csv(data_path)
+print("Salary column in modified_census.csv")
+print("salary" in data.columns)
 train, _ = train_test_split(data, test_size=0.20)
 X, y, _, _ = process_data(
                           train,

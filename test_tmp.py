9c8c267ea8463e50d4c7fc074e56cb7f3a1f9315
Waqas Bukahri <waqas07_engr@yahoo.com>
2022-02-08 14:52:47 +0300

    Update test_tmp.py

--- test_tmp.py
+++ test_tmp.py
@@ -2,7 +2,11 @@ import os
 import pandas as pd
 
 data_path = os.path.join(os.getcwd(), r"modified_census.csv")
+print(os.getcwd())
+print()
 data = pd.read_csv(data_path)
+print(data)
+print()
 print(data.shape)
 print(data.head())
 print()

0921d95e35e8d6b3a0dbd8f249400095448b1af5
Waqas Bukahri <waqas07_engr@yahoo.com>
2022-02-11 15:24:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
-    # os.system("rm -r .dvc .apt/usr/lib/dvc")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 @app.on_event("startup")

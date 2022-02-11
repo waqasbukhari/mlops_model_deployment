1ce233e94f0f4a77cf959b3a6bffd8efe292badb
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-02-11 07:09:29 +0300

    some changes

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ app = FastAPI()
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    os.system("dvc remote add -df s3remote s3://udacity-student-waqas")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
